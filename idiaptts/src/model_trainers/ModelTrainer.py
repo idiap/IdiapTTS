Fixing iteration computation for scheduler when use_loaded_learning_rate=False.
Former-commit-id: dfcebd8a7a6c9180d03ddb29e6736ba96dacc63b

idiaptts/src/model_trainers/ModelTrainer.py
@@ -128,7 +128,7 @@ class ModelTrainer(object):
 
         self.loss_function = None  # Has to be defined by subclass.
 
-        self.current_epoch = None
+        self.total_epoch = None  # Total number of epochs the current model was trained.
 
     @staticmethod
     def create_hparams(hparams_string=None, verbose=False):
@@ -334,9 +334,9 @@ class ModelTrainer(object):
             # Try to load the model. If it doesn't exist, create a new one and save it.
             # Return the loaded/created model, because no training was requested.
             try:
-                self.current_epoch = self.model_handler.load_checkpoint(hparams.model_path,
-                                                                        hparams,
-                                                                        hparams.optimiser_args["lr"] if hasattr(hparams, "optimiser_args")
+                self.total_epoch = self.model_handler.load_checkpoint(hparams.model_path,
+                                                                      hparams,
+                                                                      hparams.optimiser_args["lr"] if hasattr(hparams, "optimiser_args")
                                                                                                      and "lr" in hparams.optimiser_args
                                                                                                      else 0.0)
             except FileNotFoundError:
@@ -347,22 +347,22 @@ class ModelTrainer(object):
                     self.logger.warning('Model does not exist at {}. Creating a new one instead and saving it.'.format(hparams.model_path))
                     dim_in, dim_out = self.dataset_train.get_dims()
                     self.model_handler.create_model(hparams, dim_in, dim_out)
-                    self.current_epoch = 0
-                    self.model_handler.save_checkpoint(model_path_out, self.current_epoch)
+                    self.total_epoch = 0
+                    self.model_handler.save_checkpoint(model_path_out, self.total_epoch)
 
             self.logger.info("Model ready.")
             return
 
         if hparams.model_type is None:
-            self.current_epoch = self.model_handler.load_checkpoint(hparams.model_path,
-                                                                    hparams,
-                                                                    hparams.optimiser_args["lr"] if hasattr(hparams, "optimiser_args")
+            self.total_epoch = self.model_handler.load_checkpoint(hparams.model_path,
+                                                                  hparams,
+                                                                  hparams.optimiser_args["lr"] if hasattr(hparams, "optimiser_args")
                                                                                                  and "lr" in hparams.optimiser_args
                                                                                                  else 0.0)
         else:
             dim_in, dim_out = self.dataset_train.get_dims()
             self.model_handler.create_model(hparams, dim_in, dim_out)
-            self.current_epoch = 0
+            self.total_epoch = 0
 
         self.logger.info("Model ready.")
 
@@ -431,43 +431,42 @@ class ModelTrainer(object):
 
         t_start = timer()
         self.logger.info('Start training: ' + str(datetime.now()))
-        # all_loss, all_loss_train = self.model_handler.run(hparams, self.loss_function)
 
         self.model_handler.set_optimiser(hparams)
-        self.model_handler.set_scheduler(hparams, self.current_epoch)
+        self.model_handler.set_scheduler(hparams, self.total_epoch if hparams.use_saved_learning_rate else 0)
 
         assert(self.loss_function)  # Please set self.loss_function in the trainer construction.
-        self.model_handler.loss_function = self.loss_function.cuda() if hparams.use_gpu else self.loss_function
+        loss_function = self.loss_function.cuda() if hparams.use_gpu else self.loss_function
 
         all_loss = list()  # List which is returned, containing all loss so that progress is visible.
         all_loss_train = list()
         best_loss = np.nan
-        start_epoch = self.current_epoch
+        start_epoch = self.total_epoch
 
         # Compute error before first iteration.
         if hparams.start_with_test:
-            self.logger.info('Test epoch [{}/{}]:'.format(self.current_epoch, start_epoch + hparams.epochs))
-            loss, loss_features = self.model_handler.test(hparams, self.current_epoch)
+            self.logger.info('Test epoch [{}/{}]:'.format(start_epoch, start_epoch + hparams.epochs))
+            loss, loss_features = self.model_handler.test(hparams, start_epoch, start_epoch, loss_function)
             all_loss_train.append(-1.0)  # Set a placeholder at the train losses.
             all_loss.append(loss)
             best_loss = loss  # Variable to save the current best loss.
 
-        for _ in range(hparams.epochs):
+        for current_epoch in range(1, hparams.epochs + 1):
             # Increment epoch number.
-            self.current_epoch += 1
+            self.total_epoch += 1
 
             # Train one epoch.
-            self.logger.info('Train epoch [{}/{}]:'.format(self.current_epoch, start_epoch + hparams.epochs))
-            train_loss = self.model_handler.train(hparams, self.current_epoch, self.loss_function)
+            self.logger.info('Train epoch [{}/{}]:'.format(self.total_epoch, start_epoch + hparams.epochs))
+            train_loss = self.model_handler.train(hparams, self.total_epoch, current_epoch, loss_function)
             all_loss_train.append(train_loss)
             if np.isnan(train_loss):
                 break
 
             # Test if requested.
-            if self.current_epoch % hparams.epochs_per_test == 0:
-                self.logger.info('Test epoch [{}/{}]:'.format(self.current_epoch, start_epoch + hparams.epochs))
+            if self.total_epoch % hparams.epochs_per_test == 0:
+                self.logger.info('Test epoch [{}/{}]:'.format(self.total_epoch, start_epoch + hparams.epochs))
                 # Compute error on validation set.
-                loss, loss_features = self.model_handler.test(hparams, self.current_epoch)
+                loss, loss_features = self.model_handler.test(hparams, self.total_epoch, current_epoch, loss_function)
 
                 # Save loss in a list which is returned.
                 all_loss.append(loss)
@@ -480,18 +479,20 @@ class ModelTrainer(object):
                 if hparams.out_dir is not None:
                     path_checkpoint = os.path.join(hparams.out_dir, hparams.networks_dir, hparams.checkpoints_dir)
                     # Check when to save a checkpoint.
-                    if hparams.epochs_per_checkpoint > 0 and self.current_epoch % hparams.epochs_per_checkpoint == 0:
-                        model_name = "{}-e{}-{}".format(hparams.model_name, self.current_epoch, self.loss_function)
-                        self.model_handler.save_checkpoint(os.path.join(path_checkpoint, model_name), self.current_epoch)
+                    if hparams.epochs_per_checkpoint > 0 and self.total_epoch % hparams.epochs_per_checkpoint == 0:
+                        model_name = "{}-e{}-{}".format(hparams.model_name, self.total_epoch, loss_function)
+                        self.model_handler.save_checkpoint(os.path.join(path_checkpoint, model_name), self.total_epoch)
                     # Always save best checkpoint with special name.
                     if loss < best_loss or np.isnan(best_loss):
                         best_loss = loss
                         model_name = hparams.model_name + "-best"
-                        self.model_handler.save_checkpoint(os.path.join(path_checkpoint, model_name), self.current_epoch)
+                        self.model_handler.save_checkpoint(os.path.join(path_checkpoint, model_name), self.total_epoch)
 
                 # Run the scheduler if requested.
-                if hparams.epochs_per_scheduler_step and self.current_epoch % hparams.epochs_per_scheduler_step == 0:
-                    self.model_handler.run_scheduler(loss, self.current_epoch + 1)
+                if hparams.epochs_per_scheduler_step:
+                    if (self.total_epoch if hparams.use_saved_learning_rate else current_epoch)\
+                            % hparams.epochs_per_scheduler_step == 0:
+                        self.model_handler.run_scheduler(loss, self.total_epoch + 1)
 
         t_training = timer() - t_start
         self.logger.info('Training time: ' + str(timedelta(seconds=t_training)))
@@ -503,22 +504,22 @@ class ModelTrainer(object):
             if hparams.use_best_as_final_model:
                 best_model_path = os.path.join(hparams.out_dir, hparams.networks_dir, hparams.checkpoints_dir, hparams.model_name + "-best")
                 try:
-                    self.current_epoch = self.model_handler.load_checkpoint(best_model_path,
-                                                                            hparams,
-                                                                            hparams.optimiser_args["lr"] if hparams.optimiser_args["lr"]
+                    self.total_epoch = self.model_handler.load_checkpoint(best_model_path,
+                                                                          hparams,
+                                                                          hparams.optimiser_args["lr"] if hparams.optimiser_args["lr"]
                                                                                                          else hparams.learning_rate)
                     if self.model_handler.ema:  # EMA model should be used as best model.
                         self.model_handler.model = self.model_handler.ema.model
                         self.model_handler.ema = None  # Reset this one so that a new one is created for further training.
-                        self.logger.info("Using best EMA model (epoch {}) as final model.".format(self.current_epoch))
+                        self.logger.info("Using best EMA model (epoch {}) as final model.".format(self.total_epoch))
                     else:
-                        self.logger.info("Using best (epoch {}) as final model.".format(self.current_epoch))
+                        self.logger.info("Using best (epoch {}) as final model.".format(self.total_epoch))
                 except FileNotFoundError:
                     self.logger.warning("No best model exists yet. Continue with the current one.")
 
             # Save the model if requested.
             if hparams.save_final_model:
-                self.model_handler.save_checkpoint(os.path.join(hparams.out_dir, hparams.networks_dir, hparams.model_name), self.current_epoch)
+                self.model_handler.save_checkpoint(os.path.join(hparams.out_dir, hparams.networks_dir, hparams.model_name), self.total_epoch)
 
         return all_loss, all_loss_train, self.model_handler
 

idiaptts/src/neural_networks/pytorch/ModelHandlerPyTorch.py
@@ -235,20 +235,32 @@ class ModelHandlerPyTorch(ModelHandler):
                     hparams.epochs_per_scheduler_step = 1
                 return
 
-            elif hparams.scheduler_type == "Exponential":
-                self.scheduler = ExponentialLR(self.optimiser, last_epoch=current_epoch - 1, **hparams.scheduler_args)
+            current_iteration = max((current_epoch - 1) * len(self.dataloader_train), -1)
+            if hparams.scheduler_type == "Exponential":
+                if hparams.epochs_per_scheduler_step is None:
+                    if hparams.iterations_per_scheduler_step is None:
+                        hparams.iterations_per_scheduler_step = 1
+                    self.scheduler = ExponentialLR(self.optimiser, last_epoch=current_iteration, **hparams.scheduler_args)
+                else:
+                    self.scheduler = ExponentialLR(self.optimiser, last_epoch=current_epoch - 1, **hparams.scheduler_args)
                 self._scheduler_step_fn = self._scheduler_step
-                if hparams.epochs_per_scheduler_step is None and hparams.iterations_per_scheduler_step is None:
-                    hparams.iterations_per_scheduler_step = 1
                 return
 
             elif hparams.scheduler_type == "Noam":
-                if "wormup_steps" not in hparams.scheduler_args:
-                    self.logger.error("Please define wormup_steps in hparams.scheduler_args.")
-                self.scheduler = LambdaLR(self.optimiser, lambda iteration: float(hparams.scheduler_args['wormup_steps'])**0.5 * np.minimum((iteration + 1) * float(hparams.scheduler_args['wormup_steps'])**-1.5, (iteration + 1)**-0.5))
+                assert hasattr(hparams.scheduler_args, "wormup_steps"), "Please define wormup_steps in hparams.scheduler_args."
+
+                def noam_decay(iteration):
+                    wormup_steps = float(hparams.scheduler_args['wormup_steps'])
+                    return wormup_steps**0.5 * np.minimum((iteration + 1) * wormup_steps**-1.5,
+                                                          (iteration + 1)**-0.5)
+
+                if hparams.epochs_per_scheduler_step is None:
+                    if hparams.iterations_per_scheduler_step is None:
+                        hparams.iterations_per_scheduler_step = 1
+                    self.scheduler = LambdaLR(self.optimiser, noam_decay, last_epoch=current_iteration)
+                else:
+                    self.scheduler = LambdaLR(self.optimiser, noam_decay, last_epoch=current_epoch - 1)
                 self._scheduler_step_fn = self._scheduler_step
-                if hparams.epochs_per_scheduler_step is None and hparams.iterations_per_scheduler_step is None:
-                    hparams.iterations_per_scheduler_step = 1
                 return
 
             # TODO: Implement the others here.
@@ -398,14 +410,14 @@ class ModelHandlerPyTorch(ModelHandler):
 
         return checkpoint['epoch']
 
-    def save_checkpoint(self, file_path, current_epoch):
+    def save_checkpoint(self, file_path, total_epoch):
         """
         Save checkpoint which consists of epoch number, model type, in/out dimensions, model state dict, whole
         optimiser, etc. Also save the EMA separately when one exists.
         """
         self.logger.info("Save checkpoint to " + file_path)
         makedirs_safe(os.path.dirname(file_path))  # Create directory if necessary.
-        checkpoint_dict = {'epoch': current_epoch,
+        checkpoint_dict = {'epoch': total_epoch,
                            'model_name': self.model_name,
                            'optimiser': self.optimiser
                            # 'loss_function': loss_function
@@ -484,13 +496,17 @@ class ModelHandlerPyTorch(ModelHandler):
         else:
             return return_values.detach().numpy()
 
-    def process_dataloader(self, dataloader, hparams, current_epoch, training=True):
+    def process_dataloader(self, dataloader, loss_function, hparams, total_epoch, current_epoch=None, training=True):
         """
         Train or test the model by loading batches from the dataloader.
 
         :param dataloader:        Dataloader of the train/test set.
+        :param loss_function:     PyTorch function/class to compute loss.
         :param hparams:           Hyper-parameter container.
-        :param current_epoch:     Current training epoch. Used to compute the current iteration for some schedulers.
+        :param total_epoch:       Total number of training epochs. Used to compute the iteration for some schedulers
+                                  when the learning rate is not reset in beginning of current train loop.
+        :param current_epoch:     Number of epoch in current training loop. Used to compute the iteration for some
+                                  schedulers when the learning rate was reset in beginning of current train loop
         :param training:          Determines if it runs the training or testing loop.
         :return:                  Tuple of total loss and total loss per output feature.
         """
@@ -594,7 +610,7 @@ class ModelHandlerPyTorch(ModelHandler):
                                                seq_lengths_target)
 
             # Compute loss of the output.
-            loss_full = self.loss_function(output, target)
+            loss_full = loss_function(output, target)
             assert(loss_full.nelement() > 1)  # Don't reduce the loss, so that the mask can be applied. Use reduction='none' in loss function.
             if mask is not None:
                 loss_full = loss_full * mask  # Don't do inplace multiplication because both tensors could be expanded.
@@ -666,10 +682,15 @@ class ModelHandlerPyTorch(ModelHandler):
                     self.ema.update_params(model)
 
                 # Run the scheduler_type if one exists and should be called after some iterations.
-                if self.scheduler is not None:
-                    if hparams.iterations_per_scheduler_step is not None and (current_batch_index + 1) % hparams.iterations_per_scheduler_step == 0:
-                        self._scheduler_step_fn(loss.detach(), (current_epoch - 1) * len(dataloader) + current_batch_index + 1)
-                        # self.logger.info(str(self.optimiser))
+                if self.scheduler:
+                    if hparams.iterations_per_scheduler_step:
+                        if hparams.use_saved_learning_rate:
+                            current_iteration = (total_epoch - 1) * len(dataloader) + current_batch_index + 1
+                        else:
+                            current_iteration = (current_epoch - 1) * len(dataloader) + current_batch_index + 1
+                        if current_iteration % hparams.iterations_per_scheduler_step == 0:
+                            self._scheduler_step_fn(loss.detach(), current_iteration)
+                            # self.logger.info(str(self.optimiser))
 
             # Logging current error.
             if current_batch_index % logging_batch_index == 0:
@@ -725,13 +746,18 @@ class ModelHandlerPyTorch(ModelHandler):
 
         return np_total_loss, np_loss_features
 
-    def test(self, hparams, current_epoch):
+    def test(self, hparams, total_epoch, current_epoch, loss_function):
         if hparams.use_gpu:
             assert (hparams.num_gpus <= torch.cuda.device_count())  # Specified number of GPUs is incorrect.
 
-        return self.process_dataloader(self.dataloader_val, hparams, current_epoch, training=False)
+        return self.process_dataloader(self.dataloader_val,
+                                       loss_function,
+                                       hparams,
+                                       total_epoch,
+                                       current_epoch,
+                                       training=False)
 
-    def train(self, hparams, current_epoch, loss_function):
+    def train(self, hparams, total_epoch, current_epoch, loss_function):
         if hparams.use_gpu:
             assert (hparams.num_gpus <= torch.cuda.device_count())  # Specified number of GPUs is incorrect.
 
@@ -740,7 +766,12 @@ class ModelHandlerPyTorch(ModelHandler):
             average_model.load_state_dict(self.model.state_dict())
             self.ema = ExponentialMovingAverage(average_model, hparams.ema_decay)
 
-        return self.process_dataloader(self.dataloader_train, hparams, current_epoch)[0]
+        return self.process_dataloader(self.dataloader_train,
+                                       loss_function,
+                                       hparams,
+                                       total_epoch,
+                                       current_epoch,
+                                       training=True)[0]
 
     def run_scheduler(self, loss, current_epoch):
         if self.scheduler is not None: